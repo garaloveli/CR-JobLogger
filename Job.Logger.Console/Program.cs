ba423d35c95554838f2cf9ffc31cb275b54887fd
Olga Zegarra <garaloveli@gmail.com>
2017-11-18 19:04:39 -0500

    Initial Commit

---
+++ Job.Logger.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Job.Logger.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
